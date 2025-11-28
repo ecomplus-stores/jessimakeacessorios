75cb881711230b6d772776c9ed165277cf00dc33
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-11-28 14:32:12 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,16 +1,28 @@
 // Add your custom JavaScript for checkout here.
 storefront.on('widget:@ecomplus/widget-tag-manager', function () {
-    document.querySelector('.cart__btn-checkout').insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
-          Eu li e aceito
-          a
+  // Inserir checkbox antes do botão
+  document.querySelector('.cart__btn-checkout').insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+        <label for="input-confirm-checkout" class="custom-control-label">
+          Eu li e aceito a
           <a href="/pages/termos-de-aceite" target="_blank">Termos de Uso</a> para continuar comprando
-        </label></div></div>
-    `);
-    document.querySelector('#block-confirm').addEventListener('click', (e) => {
-        if (e.target.checked) {
-            document.querySelector('#block-confirm').style.display = 'none'
-            document.querySelector('.cart__btn-checkout').style.display = 'block'
-        }
-    })
+        </label>
+      </div>
+    </div>
+  `)
+
+  // Desabilita o botão até aceitar
+  const btn = document.querySelector('.cart__btn-checkout')
+  btn.setAttribute('disabled', true)
+
+  // Habilita quando marcado
+  document.querySelector('#input-confirm-checkout').addEventListener('change', function () {
+    if (this.checked) {
+      btn.removeAttribute('disabled')
+    } else {
+      btn.setAttribute('disabled', true)
+    }
+  })
 })
